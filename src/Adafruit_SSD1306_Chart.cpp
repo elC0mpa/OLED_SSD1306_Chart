d214dce4700bbbcaadb5c8b8f803daacd6b4c9f6
elC0mpa <gcompanionib@gmail.com>
2020-01-04 10:49:26 +0100

    First Commit

---
+++ src/Adafruit_SSD1306_Chart.cpp
@@ -0,0 +1,126 @@
+#include "Adafruit_SSD1306_Chart.h"
+
+/*!
+    @brief  Updates the internal buffer to draw the cartesian graph
+    @note   Call the object's begin() function before use -- buffer allocation is performed there!
+            Call the object's configureChart() function before use -- params are updated there
+*/
+void Adafruit_SSD1306_Chart::drawChart()
+{
+    double i, temp;
+    _dig = 0;
+    _ox[0] = _gx;
+    _ox[1] = _gx;
+    _oy[0] = _gy;
+    _oy[1] = _gy + _h/2;
+    _x = _gx;
+
+
+    for (i = _gy; i <= _gy + _h; i += _yincdiv) {
+    temp =  (i - _gy) * (_gy - _h - _gy) / (_h ) + _gy;
+    if (i == _gy)
+    {
+        drawFastHLine(_gx - 3, temp, _w + 3, WHITE);
+    } 
+    else 
+    {
+        drawFastHLine(_gx - 3, temp, 3, WHITE);
+    }
+    }
+    // draw x scale
+    for (i = 0; i <= _w; i += _xincdiv) {
+    temp =  (i) + _gx;
+    if (i == 0) {
+        drawFastVLine(temp, _gy - _h, _h + 3, WHITE);
+    }
+    else {
+        drawFastVLine(temp, _gy, 3, WHITE);
+    }
+    }
+}
+
+void Adafruit_SSD1306_Chart::setPlotMode(char mode)
+{
+    if (mode == SINGLE_MODE || mode == DOUBLE_MODE)
+        _mode = mode;
+}
+
+void Adafruit_SSD1306_Chart::setYLimits(double ylo, double yhi, uint8_t chart)
+{
+    if(chart == 0 || chart == 1)
+    {
+        _ylo[chart] = ylo;
+        _yhi[chart] = yhi;
+    }
+}
+
+void Adafruit_SSD1306_Chart::setChartCoordinates(double x, double y)
+{
+    _gx = x;
+    _gy = y;
+}
+
+void Adafruit_SSD1306_Chart::setChartWidthAndHeight(double w, double h)
+{
+    _w = w;
+    _h = h;
+}
+
+void Adafruit_SSD1306_Chart::setAxisDivisionsInc(double xinc, double yinc)
+{
+    _xincdiv = xinc;
+    _yincdiv = yinc;
+}
+
+void Adafruit_SSD1306_Chart::setXIncrement(double xinc)
+{
+    _xinc = xinc;
+}
+
+bool Adafruit_SSD1306_Chart::updateChart(double firstValue, double secondValue)
+{
+    if (_x >= _gx + _w)
+        return false;
+
+    _x+=_xinc;
+
+    
+    if (_mode == SINGLE_MODE)
+    {
+        double y =  (firstValue - _ylo[0]) * (_gy - _h - _gy) / (_yhi[0] - _ylo[0]) + _gy;
+        drawLine(_ox[0], _oy[0], _x, y, WHITE);
+        drawLine(_ox[0], _oy[0] - 1, _x, y - 1, WHITE); 
+
+        _ox[0] = _x;
+        _oy[0] = y;
+
+        fillCircle(_ox[0], _oy[0], 2, WHITE);
+        
+        display();
+        return true;
+    }
+
+    else if(_mode == DOUBLE_MODE)
+    {
+        auto semiHeight = _h / 2;
+        double y =  (firstValue - _ylo[0]) * (- semiHeight) / (_yhi[0] - _ylo[0]) + _gy;
+        double secondY = (secondValue - _ylo[1]) * (- semiHeight ) / (_yhi[0] - _ylo[0]) + _gy - semiHeight; 
+
+        drawLine(_ox[0], _oy[0], _x, y, WHITE);
+        drawLine(_ox[0], _oy[0] - 1, _x, y - 1, WHITE); 
+
+        drawLine(_ox[1], _oy[1], _x, secondY, WHITE);
+        drawLine(_ox[1], _oy[1] - 1, _x, secondY - 1, WHITE); 
+
+        _ox[0] = _x;
+        _oy[0] = y;
+        _ox[1] = _x;
+        _oy[1] = secondY;
+
+        fillCircle(_ox[0], _oy[0], 2, WHITE);
+        fillCircle(_ox[1], _oy[1], 2, WHITE);
+
+        display();
+        return true;
+    }
+}
---
+++ src/Adafruit_SSD1306_Chart.h
@@ -0,0 +1,41 @@
+#include "Adafruit_SSD1306_SlowSoftWire.h"
+
+#define SINGLE_MODE 0
+#define DOUBLE_MODE 1
+
+class Adafruit_SSD1306_Chart:public Adafruit_SSD1306
+{
+private:
+    double _ox[2], _oy[2];            //Previous point coordinates
+    double _gx, _gy;            //Chart lower left coordinates
+    double _w, _h;              //Chart width and height
+    double _ylo[2], _yhi[2];          //Y axis Min and max values
+    double _xinc;               //X coordinate increment between values
+    double _x;                  //Actual point x coordinate
+    double _xincdiv, _yincdiv;  //X and Y axis distance between division
+    double _dig;                
+    char _mode;                 //Plot mode: single or double
+public:
+
+    //Ctors
+    Adafruit_SSD1306_Chart():Adafruit_SSD1306(){_mode = SINGLE_MODE;}
+
+    Adafruit_SSD1306_Chart(uint8_t w, uint8_t h, SlowSoftWire *twi, int8_t rst_pin=-1,
+        uint32_t clkDuring=400000UL, uint32_t clkAfter=100000UL):Adafruit_SSD1306(w, h, twi, rst_pin, clkDuring, clkAfter){_mode = SINGLE_MODE;}
+
+    Adafruit_SSD1306_Chart(uint8_t w, uint8_t h, int8_t mosi_pin, int8_t sclk_pin,
+        int8_t dc_pin, int8_t rst_pin, int8_t cs_pin):Adafruit_SSD1306(w, h, mosi_pin, sclk_pin, dc_pin, rst_pin, cs_pin){_mode = SINGLE_MODE;}
+
+    Adafruit_SSD1306_Chart(uint8_t w, uint8_t h, SPIClass *spi,
+        int8_t dc_pin, int8_t rst_pin, int8_t cs_pin, uint32_t bitrate=8000000UL):Adafruit_SSD1306(w, h, spi, dc_pin, rst_pin, cs_pin, bitrate){_mode = SINGLE_MODE;}
+
+    void setPlotMode(char mode);
+    void setChartCoordinates(double x, double y);
+    void setChartWidthAndHeight(double w, double h);
+    void setYLimits(double ylo, double yhi, uint8_t chart = 0);
+    void setXIncrement(double xinc);
+    void setAxisDivisionsInc(double xinc, double yinc);
+
+    void drawChart();
+    bool updateChart(double firstValue, double secondValue = 0);
+};
