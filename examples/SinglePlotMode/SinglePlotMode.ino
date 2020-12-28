b4016133752eeaa600753ebe8e105b8d2a8c1b2a
Jose Gabriel Companioni Benitez <53531665+elC0mpa@users.noreply.github.com>
2020-12-27 21:00:34 -0300

    Examples updated

--- examples/SinglePlotMode/SinglePlotMode.ino
+++ examples/SinglePlotMode/SinglePlotMode.ino
@@ -7,12 +7,13 @@
 
 #include <Arduino.h>
 #include <OLED_SSD1306_Chart.h>
+#include <Adafruit_I2CDevice.h> //Include this to avoid compile errors in Platformio
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define OLED_RESET -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 
 #define BAUDRATE 9600
 
@@ -20,33 +21,39 @@
 #define SCL_PIN D2
 
 OLED_SSD1306_Chart display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
- 
-void setup() {
-  // put your setup code here, to run once:
-  #if defined ESP8266
+
+void setup()
+{
+// put your setup code here, to run once:
+#if defined ESP8266
   Wire.begin(SDA_PIN, SCL_PIN);
-  #else
+#else
   Wire.begin();
-  #endif
+#endif
 
   display.begin(SSD1306_SWITCHCAPVCC, 0x3c);
   display.clearDisplay();
-  display.setChartCoordinates(0, 60);         //Chart lower left coordinates (X, Y)
-  display.setChartWidthAndHeight(123, 60);    //Chart width = 123 and height = 60
-  display.setXIncrement(5);                   //Distance between Y points will be 5px
-  display.setYLimits(0, 100);                 //Ymin = 0 and Ymax = 100
-  display.setAxisDivisionsInc(12, 6);         //Each 12 px a division will be painted in X axis and each 6px in Y axis
-  display.setPlotMode(SINGLE_PLOT_MODE);      //Set single plot mode
-  display.drawChart();                        //Update the buffer to draw the cartesian chart 
+  display.setChartCoordinates(0, 60);      //Chart lower left coordinates (X, Y)
+  display.setChartWidthAndHeight(123, 55); //Chart width = 123 and height = 60
+  display.setXIncrement(5);                //Distance between Y points will be 5px
+  display.setYLimits(0, 100);              //Ymin = 0 and Ymax = 100
+  display.setYLimitLabels("0", "100");     //Setting Y axis labels
+  display.setYLabelsVisible(true);
+  display.setAxisDivisionsInc(12, 6);    //Each 12 px a division will be painted in X axis and each 6px in Y axis
+  display.setPlotMode(SINGLE_PLOT_MODE); //Set single plot mode
+  display.setPointGeometry(POINT_GEOMETRY_CIRCLE);
+  display.drawChart(); //Update the buffer to draw the cartesian chart
   display.display();
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
   auto value = random(100);
-  if(!display.updateChart(value))             //Value between Ymin and Ymax will be added to chart
-    {
-      display.clearDisplay();                 //If chart is full, it is drawn again
-      display.drawChart();
-    }
+  if (!display.updateChart(value)) //Value between Ymin and Ymax will be added to chart
+  {
+    display.clearDisplay(); //If chart is full, it is drawn again
+    display.drawChart();
+  }
+  delay(100);
 }
